f762d280826feb8e36c3e5a788614b3295652815
Quinten1508 <quinten1508@gmail.com>
2025-05-04 22:25:01 +0200

    Remove vulnerable packages and fix build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { visualizer } from 'rollup-plugin-visualizer'
-import viteImagemin from 'vite-plugin-imagemin'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -18,33 +17,6 @@ export default defineConfig(({ mode }) => ({
       gzipSize: true,
       brotliSize: true,
       filename: 'dist/stats.html',
-    }),
-    viteImagemin({
-      gifsicle: {
-        optimizationLevel: 7,
-        interlaced: false,
-      },
-      optipng: {
-        optimizationLevel: 7,
-      },
-      mozjpeg: {
-        quality: 80,
-      },
-      pngquant: {
-        quality: [0.8, 0.9],
-        speed: 4,
-      },
-      svgo: {
-        plugins: [
-          {
-            name: 'removeViewBox',
-          },
-          {
-            name: 'removeEmptyAttrs',
-            active: false,
-          },
-        ],
-      },
     }),
   ].filter(Boolean),
   resolve: {
