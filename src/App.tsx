0e6e9a852342ad944296711f9028d409239cb4de
Quinten1508 <quinten1508@gmail.com>
2025-05-04 23:13:03 +0200

    hosting fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui-optimized/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import routes from "./routes";
 import CursorEffectsProvider from "./components/ui/CursorEffectsProvider";
 
@@ -19,14 +19,14 @@ const App = () => (
       <CursorEffectsProvider>
         <Toaster />
         <Sonner />
-        <HashRouter>
+        <BrowserRouter>
           <Suspense fallback={<div className="flex items-center justify-center h-screen">Loading...</div>}>
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/presentation" element={<Presentation />} />
             </Routes>
           </Suspense>
-        </HashRouter>
+        </BrowserRouter>
       </CursorEffectsProvider>
     </TooltipProvider>
   </QueryClientProvider>
